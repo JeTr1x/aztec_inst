11d5b50442b7a387a9c4535030dab6e19b8af631
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-08 02:48:54 +0300

    Update and rename remove_all.sh to azt_remove_all.sh

---
+++ azt_remove_all.sh
@@ -0,0 +1,19 @@
+
+
+cd
+cd aztec_node
+docker compose down --remove-orphans
+
+cd
+cd aztec_agent
+docker compose down --remove-orphans
+
+cd
+cd aztec_broker
+docker compose down --remove-orphans
+
+cd
+cd prover
+docker compose down --remove-orphans
+
+rm -rf aztec_node aztec_broker aztec_agent
