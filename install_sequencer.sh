6c3757b48df4a0e85a9d5bac60feb38c10f1cae5
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-08 20:45:26 +0300

    Update install_sequencer.sh

--- install_sequencer.sh
+++ install_sequencer.sh
@@ -1,12 +1,13 @@
 
 PK=$1
+TAG=$2 # например 1.2.1  - без V
 IP=$(curl 2ip.ru)
 
 mkdir aztec-sequencer-node
 cd aztec-sequencer-node
 
 wget -O docker-compose.yml https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/sequencer-compose.yml
-
+sed -i s"|aztecprotocol/aztec:latest|aztecprotocol/aztec:${TAG}|" docker-compose.yml
 
 sudo tee .env > /dev/null <<EOF
 ETHEREUM_HOSTS="http://176.9.48.61:18545"
