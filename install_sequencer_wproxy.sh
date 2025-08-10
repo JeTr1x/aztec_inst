5f1818c15333ab7f3a0d8d184c53a8992312eaa0
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-10 18:44:15 +0300

    Update install_sequencer_wproxy.sh

--- install_sequencer_wproxy.sh
+++ install_sequencer_wproxy.sh
@@ -9,7 +9,7 @@ IP=$(curl 2ip.ru)
 mkdir aztec-sequencer-node
 cd aztec-sequencer-node
 
-wget -O docker-compose.yml https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/sequencer-compose.yml
+wget -O docker-compose.yml https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/sequencer-proxy-compose.yml
 sed -i s"|aztecprotocol/aztec:latest|aztecprotocol/aztec:${TAG}|" docker-compose.yml
 
 sudo tee .env > /dev/null <<EOF
