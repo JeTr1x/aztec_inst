f20547fcf6866fa6679dba31f6906b1c3db55d99
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-10 14:51:31 +0300

    Create install_sequencer_wproxy.sh

---
+++ install_sequencer_wproxy.sh
@@ -0,0 +1,22 @@
+
+PK=$1
+PROX=$2
+TAG=$3 # например 1.2.1  - без V
+IP=$(curl 2ip.ru)
+
+mkdir aztec-sequencer-node
+cd aztec-sequencer-node
+
+wget -O docker-compose.yml https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/sequencer-compose.yml
+sed -i s"|aztecprotocol/aztec:latest|aztecprotocol/aztec:${TAG}|" docker-compose.yml
+
+sudo tee .env > /dev/null <<EOF
+ETHEREUM_HOSTS="http://176.9.48.61:18545"
+L1_CONSENSUS_HOST_URLS="http://176.9.48.61:13500"
+VALIDATOR_PRIVATE_KEY=${PK}
+P2P_IP=${IP}
+PROXY=${PROX}
+EOF
+
+docker compose up -d
+docker compose logs -f
