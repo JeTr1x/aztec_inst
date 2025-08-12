ff3aa0742d659620c99a3cae0a9e146652c9cb3a
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-13 00:21:17 +0300

    Update and rename fix_p2p.sh to fix_p2pip.sh

--- fix_p2pip.sh
+++ fix_p2pip.sh
@@ -5,7 +5,7 @@ IP=$(echo "$PROX" | sed -E 's|.*@([^:]+):.*|\1|')
 
 cd aztec-sequencer-node-${ID}
 
-sed -i -E "s/^(P2P_IP=)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/\1${IP}/" .env
+sed -i -E "s/^(P2P_IP=).*/\1${IP}/"  .env
 
 docker compose down
 docker compose up -d
