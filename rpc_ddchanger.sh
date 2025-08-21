3e5707c966e5a18634c1c4727b55d954b184221a
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-21 22:18:55 +0300

    Update and rename rpc_dchanger.sh to rpc_ddchanger.sh

--- rpc_ddchanger.sh
+++ rpc_ddchanger.sh
@@ -3,9 +3,8 @@ NEWRPC=$2
 NEWCONS=$3
 
 
-cd aztec-sequencer-node-${ID}
+cd aztec-sequencer-ipnode-${ID}
 docker compose down
-wget -O docker-compose.yml https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/sequencer-proxy-compose1.2.1.yml
 sed -i -E "s|^(ETHEREUM_HOSTS=).*|\1${NEWRPC}|"  .env
 sed -i -E "s|^(L1_CONSENSUS_HOST_URLS=).*|\1${NEWCONS}|"  .env
 
