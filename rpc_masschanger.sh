e17e0babb85647b120e8a61093df71caa9f1c77c
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-21 22:21:06 +0300

    Update and rename rpc_ddchanger.sh to rpc_masschanger.sh

---
+++ rpc_masschanger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NEWRPC=$1
+NEWCONS=$2
+
+for ID in $(seq 1 28); do
+  echo "Обновляю узел $ID..."
+
+  cd "aztec-sequencer-ipnode-${ID}" || { echo "Нет папки aztec-sequencer-ipnode-${ID}"; exit 1; }
+
+  docker compose down
+
+  sed -i -E "s|^(ETHEREUM_HOSTS=).*|\1${NEWRPC}|" .env
+  sed -i -E "s|^(L1_CONSENSUS_HOST_URLS=).*|\1${NEWCONS}|" .env
+
+  docker compose up -d
+
+  cd ..
+done
