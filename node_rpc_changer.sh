ac94057ac269091d3da1920fd13185a9cd69d00f
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-08-22 16:40:58 +0300

    Create node_rpc_changer.sh

---
+++ node_rpc_changer.sh
@@ -0,0 +1,10 @@
+NEWRPC=$2
+NEWCONS=$3
+
+cd aztec_node
+
+sed -i -E "s|^(ETHEREUM_HOSTS=).*|\1${NEWRPC}|"  .env
+sed -i -E "s|^(L1_CONSENSUS_HOST_URLS=).*|\1${NEWCONS}|"  .env
+
+docker compose down
+docker compose up -d
