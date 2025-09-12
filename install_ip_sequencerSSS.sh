c070c662ba74e16c192cff85e166e393ab848dbc
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-09-12 14:21:09 +0300

    Update and rename install_ip_sequencerSS.sh to install_ip_sequencerSSS.sh

--- install_ip_sequencerSSS.sh
+++ install_ip_sequencerSSS.sh
@@ -2,6 +2,7 @@
 PK=$1
 ID=$2
 IP=$3
+RPCIP=$4
 
 cd aztec-sequencer-ipnode-${ID}
 docker compose down
@@ -14,8 +15,8 @@ cd aztec-sequencer-ipnode-${ID}
 wget -O docker-compose.yml https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/sequencerss-ip-compose.yml
 
 sudo tee .env > /dev/null <<EOF
-ETHEREUM_HOSTS="http://136.243.148.237:18545"
-L1_CONSENSUS_HOST_URLS="http://136.243.148.237:13500"
+ETHEREUM_HOSTS="http://${RPCIP}:18545"
+L1_CONSENSUS_HOST_URLS="http://${RPCIP}:13500"
 VALIDATOR_PRIVATE_KEY=${PK}
 DEDICATED_IP=${IP}
 ID=${ID}
