03374c99120b3a7b12b3e136032272d0f924750f
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-09-19 21:31:13 +0300

    Update install_broker.sh

--- install_broker.sh
+++ install_broker.sh
@@ -3,7 +3,6 @@ mkdir aztec_broker
 cd aztec_broker
 wget -O docker-compose.yml https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/broker-compose.yml
 wget -O .env https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/.env.broker
-sed -i s"|aztecprotocol/aztec:latest|aztecprotocol/aztec:1.2.1|" docker-compose.yml
 IP=$(curl 2ip.ru)
 sed -ie s"/P2P_IP=/P2P_IP=${IP}/" .env
 cat .env
